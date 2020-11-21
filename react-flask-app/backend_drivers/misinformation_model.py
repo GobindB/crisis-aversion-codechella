39b5c812bf197826882715fda5aa10ddb20b2748
Gobind <gobind@thesinghs.org>
2020-11-21 23:25:47 +0400

    rachels TODOs

--- react-flask-app/backend_drivers/misinformation_model.py
+++ react-flask-app/backend_drivers/misinformation_model.py
@@ -3,6 +3,8 @@
 # 1. chose which features you want from the tweet object
 # 2. create a functional model that returns a validity score as a percentage i.e 0<x<100
 # 3. In the model create evalution logic and define rules by which the model evaluates
+# 4. for the model this doesent matter but the data is stored in an AWS RDBMS in case you want 
+#    context and location specific tweets to use as data
 
 def calculate_validity_score(tweet):
     """
