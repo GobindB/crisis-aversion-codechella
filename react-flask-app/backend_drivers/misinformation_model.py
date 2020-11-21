4e86c2f02cb97cb2d7d0e4a20a068baeac0fa7e7
Gobind <gobind@thesinghs.org>
2020-11-21 23:24:25 +0400

    rachels todo misinformation_model.py

--- react-flask-app/backend_drivers/misinformation_model.py
+++ react-flask-app/backend_drivers/misinformation_model.py
@@ -1,2 +1,23 @@
+
+#TODO: @Rachel needs to build her model and use it here
+# 1. chose which features you want from the tweet object
+# 2. create a functional model that returns a validity score as a percentage i.e 0<x<100
+# 3. In the model create evalution logic and define rules by which the model evaluates
+
 def calculate_validity_score(tweet):
+    """
+    tweet parameters are passed to trained model
+    """
     return 0
+
+def train_model(tweet):
+    """
+    return: the model
+    """
+    pass 
+
+def test_model(tweet):
+    """
+    return: the model
+    """
+    pass 
