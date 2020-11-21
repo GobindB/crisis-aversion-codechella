d4f786545e073ab7cf7c64845ea1c0f001d02405
Luisa Esosteguy <luisaescosteguy@hotmail.com>
2020-11-21 14:10:15 -0600

    Add form with location and keywords

--- react-flask-app/src/views/Index.js
+++ react-flask-app/src/views/Index.js
@@ -25,7 +25,13 @@ import {
   Button,
   Container,
   Row,
-  Col
+  Col,
+  FormGroup,
+  Form,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
 } from "reactstrap";
 
 // core components
@@ -63,33 +69,24 @@ class Landing extends React.Component {
                   <Row>
                     <Col lg="6">
                       <h1 className="display-3 text-white">
-                        Crisis Aversion{" "}
-                        <span>TwitterU Codechella</span>
+                        Crisis Aversion <span>TwitterU Codechella</span>
                       </h1>
                       <p className="lead text-white">
-                      An application curated to crisis zones to facilitate dissemination of mission critical information from source to key partners with minimal lag time.
+                        An application curated to crisis zones to facilitate
+                        dissemination of mission critical information from
+                        source to key partners with minimal lag time.
                       </p>
                       <div className="btn-wrapper">
                         <Button
-                          className="btn-icon mb-3 mb-sm-0"
-                          color="primary"
-                          href="#"
-                        >
-                          <span className="btn-inner--icon mr-1">
-                            <i className="fa fa-user" />
-                          </span>
-                          <span className="btn-inner--text">For citizens</span>
-                        </Button>
-                        <Button
                           className="btn-white btn-icon mb-3 mb-sm-0 ml-1"
                           color="default"
-                          href="#"
+                          href="#responders"
                         >
                           <span className="btn-inner--icon mr-1">
                             <i className="fa fa-ambulance" />
                           </span>
                           <span className="btn-inner--text">
-                          For responders 
+                            For responders
                           </span>
                         </Button>
                       </div>
@@ -98,24 +95,100 @@ class Landing extends React.Component {
                 </div>
               </Container>
               {/* SVG separator */}
-              <div className="separator separator-bottom separator-skew">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                  version="1.1"
-                  viewBox="0 0 2560 100"
-                  x="0"
-                  y="0"
-                >
-                  <polygon
-                    className="fill-white"
-                    points="2560 0 2560 100 0 100"
-                  />
-                </svg>
-              </div>
             </section>
             {/* 1st Hero Variation */}
           </div>
+          <section className="section section-lg" id="responders">
+            <Container>
+              <Row className="row-grid align-items-center">
+                <Col className="order-md-2" md="6">
+                  <Form>
+                    <Row>
+                      <Col md="12">
+                        <FormGroup>
+                          <InputGroup className="mb-4">
+                            <InputGroupAddon addonType="prepend">
+                              <InputGroupText>
+                                <i className="ni ni-zoom-split-in" />
+                              </InputGroupText>
+                            </InputGroupAddon>
+                            <Input placeholder="Search location" type="text" />
+                          </InputGroup>
+                        </FormGroup>
+                      </Col>
+                    </Row>
+                    <p>Categories</p>
+                    <Row>
+                      <Col md="6">
+                        <div className="custom-control custom-checkbox mb-3">
+                          <input
+                            className="custom-control-input"
+                            id="help"
+                            type="checkbox"
+                          />
+                          <label
+                            className="custom-control-label"
+                            htmlFor="help"
+                          >
+                            Help
+                          </label>
+                        </div>
+                        <div className="custom-control custom-checkbox mb-3">
+                          <input
+                            className="custom-control-input"
+                            id="aid"
+                            type="checkbox"
+                          />
+                          <label className="custom-control-label" htmlFor="aid">
+                            Aid
+                          </label>
+                        </div>
+                      </Col>
+                      <Col md="6">
+                        <div className="custom-control custom-checkbox mb-3">
+                          <input
+                            className="custom-control-input"
+                            id="hurt"
+                            type="checkbox"
+                          />
+                          <label
+                            className="custom-control-label"
+                            htmlFor="hurt"
+                          >
+                            Hurt
+                          </label>
+                        </div>
+                        <div className="custom-control custom-checkbox mb-3">
+                          <input
+                            className="custom-control-input"
+                            id="food"
+                            type="checkbox"
+                          />
+                          <label
+                            className="custom-control-label"
+                            htmlFor="food"
+                          >
+                            Food
+                          </label>
+                        </div>
+                      </Col>
+                    </Row>
+                    <Button>Submit</Button>
+                  </Form>
+                </Col>
+                <Col className="order-md-1" md="6">
+                  <div className="pr-md-5">
+                    <h3>For responders</h3>
+                    <p>
+                      Fill out the form to get real-time information of crisis
+                      areas
+                    </p>
+                    <ul className="list-unstyled mt-5"></ul>
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </section>
           <section className="section section-lg">
             <Container>
               <Row className="row-grid align-items-center">
@@ -130,7 +203,8 @@ class Landing extends React.Component {
                   <div className="pr-md-5">
                     <h3>Our mission</h3>
                     <p>
-                    Helping individuals get access to accurate information from sources on the ground with little to no lag time. 
+                      Helping individuals get access to accurate information
+                      from sources on the ground with little to no lag time.
                     </p>
                     <ul className="list-unstyled mt-5">
                       <li className="py-2">
@@ -145,7 +219,8 @@ class Landing extends React.Component {
                           </div>
                           <div>
                             <h6 className="mb-0">
-                            Deliver information about crisis zones with confidence in validity
+                              Deliver information about crisis zones with
+                              confidence in validity
                             </h6>
                           </div>
                         </div>
@@ -161,7 +236,10 @@ class Landing extends React.Component {
                             </Badge>
                           </div>
                           <div>
-                            <h6 className="mb-0">Data visualization to help responders address the crisis</h6>
+                            <h6 className="mb-0">
+                              Data visualization to help responders address the
+                              crisis
+                            </h6>
                           </div>
                         </div>
                       </li>
@@ -177,7 +255,8 @@ class Landing extends React.Component {
                           </div>
                           <div>
                             <h6 className="mb-0">
-                              Text notifications for users who opt in (coming soon)
+                              Text notifications for users who opt in (coming
+                              soon)
                             </h6>
                           </div>
                         </div>
@@ -223,9 +302,7 @@ class Landing extends React.Component {
                     <div className="pt-4 text-center">
                       <h5 className="title">
                         <span className="d-block mb-1">Nandini Talwar</span>
-                        <small className="h6 text-muted">
-                          Web Developer
-                        </small>
+                        <small className="h6 text-muted">Web Developer</small>
                       </h5>
                     </div>
                   </div>
@@ -241,7 +318,9 @@ class Landing extends React.Component {
                     <div className="pt-4 text-center">
                       <h5 className="title">
                         <span className="d-block mb-1">Gobind Bakhshi</span>
-                        <small className="h6 text-muted">Backend Developer</small>
+                        <small className="h6 text-muted">
+                          Backend Developer
+                        </small>
                       </h5>
                     </div>
                   </div>
@@ -257,7 +336,9 @@ class Landing extends React.Component {
                     <div className="pt-4 text-center">
                       <h5 className="title">
                         <span className="d-block mb-1">Rachel Castellino</span>
-                        <small className="h6 text-muted">Backend Developer</small>
+                        <small className="h6 text-muted">
+                          Backend Developer
+                        </small>
                       </h5>
                     </div>
                   </div>
