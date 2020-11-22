62e0be860ed999fffe81690a827cfb1f7c5578e1
Gobind <gobind@thesinghs.org>
2020-11-22 09:53:06 +0400

    ML model

--- react-flask-app/backend_drivers/misinformation_model.py
+++ react-flask-app/backend_drivers/misinformation_model.py
@@ -1,25 +1,6 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 
-#TODO: @Rachel needs to build her model and use it here
-# 1. chose which features you want from the tweet object
-# 2. create a functional model that returns a validity score as a percentage i.e 0<x<100
-# 3. In the model create evalution logic and define rules by which the model evaluates
-# 4. for the model this doesent matter but the data is stored in an AWS RDBMS in case you want 
-#    context and location specific tweets to use as data
+reconstructed_model = keras.models.load_model("my_model")
 
-def calculate_validity_score(tweet):
-    """
-    tweet parameters are passed to trained model
-    """
-    return 0
-
-def train_model(tweet):
-    """
-    return: the model
-    """
-    pass 
-
-def test_model(tweet):
-    """
-    return: the model
-    """
-    pass 
